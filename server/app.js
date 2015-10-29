cbce9b325412b3a3c2c2bb416f2c1200014b5998
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-10-29 18:45:42 -0500

    fix

--- server/app.js
+++ server/app.js
@@ -3,16 +3,17 @@
  */
 var express = require('express');
 var app = express();
-
 var path = require('path');
 
-app.set('port', process.env.PORT || 5000);
 
-app.get("/*", function(req, res){
-    var file = req.params[0] || "index.html";
-    res.sendFile(path.join(__dirname, "../public", file));
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get("/*", function(request,response) {
+    var file = request.params[0] || "index.html";
+    response.sendFile(path.join(__dirname, "./public/", file));
 });
 
-app.listen(app.get('port', function(){
-    console.log("COOL RUNNINGS! ", app.get('port'));
-}));
+app.listen(app.get("port"), function(){
+    console.log("Listening...port: ", app.get('port'));
+});
